f82b40339971c257e5468a2e739c715e4751af27
Daniel Siemmeister <danielsiemmeister@gmail.com>
2020-08-21 09:36:12 +0200

    test.commit

---
+++ FirstJS.js
@@ -0,0 +1,15 @@
+let names = ['Daniel', 'Lena', 'Paul', 'Michi']
+names.push('Gabriel')
+console.log(names[4])
+
+
+let person = {
+    name = "Daniel",
+    age = 21,
+    Eyecolour = "Blue"
+};
+
+
+console.log(person.name);
+console.log(person["name"]);
+
